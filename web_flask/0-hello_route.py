69e57a515c2265ec35c53ced35dbaf9d20495159
walter-cumbucane <cumbucane.walter@gmail.com>
2024-03-27 22:25:34 +0200

    Task 0 done

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,26 +1,27 @@
 #!/usr/bin/python3
-"""
+'''
    Contains a flask application with a route that says 'Hello HBNB'
-"""
+'''
 from flask import Flask
 
 
 app = Flask(__name__)
+''' The Flask application instance'''
 app.rul_map.strict_slashes = False
 
 
 @app.route("/")
 def hello():
-    """
+    '''
         Defines a simple GET route
-    """
+    '''
     return "Hello HBNB!"
 
 
 def main():
-    """
+    '''
         Setups the application listening process
-    """
+    '''
     app.run(host="0.0.0.0", port=5000)
 
 
